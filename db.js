31e887b284716c6a1e94ea3d68483227c32205d1
sanju tamang <sanjutamang8222@gmail.com>
2024-11-10 20:36:08 +0545

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 //for local service data store garn ko la
 // mongoose.connect('mongodb://localhost:27017/nodes')
 
 //for online or clould ma data store garn ko lagi
 // mongoose.connect('mongodb+srv://sanjutamang:sanjutmg@cluster0.kgr0v.mongodb.net/')
 
-mongoose.connect('mongodb+srv://<db_username>:sanju1234tmg@cluster0.d6ncg.mongodb.net/')
-.then(()=>console.log("connected to MongoDB"))
-.catch((error)=>console.error("connection error:",error));
+// mongoose.connect('mongodb+srv://<db_username>:sanju1234tmg@cluster0.d6ncg.mongodb.net/')
+// .then(()=>console.log("connected to MongoDB"))
+// .catch((error)=>console.error("connection error:",error));
+// const mongoURL = 'mongodb://localhost:27017/nodes'; // Replace with your MongoDB URL
+// const mongoURL = 'mongodb+srv://sanjutamang:sanju1234tmg@cluster0.d6ncg.mongodb.net/'
+
+const mongoURL = process.env.MONGODB_URL;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoURL)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
 
 
 // const db = mongoose.connection;
